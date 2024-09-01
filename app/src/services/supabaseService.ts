e70e1e75a68615676c67ed62d3328cc2af9e5b6e
Higesennine <yasuhito.komano@gmail.com>
2024-09-01 01:04:32 -0400

    update

--- app/src/services/supabaseService.ts
+++ app/src/services/supabaseService.ts
@@ -6,7 +6,7 @@ const supabaseURL = "https://dwpoasnfpbdswanexiyr.supabase.co"
 const supabaseANONKEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cG9hc25mcGJkc3dhbmV4aXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyNzIxNTEsImV4cCI6MjAzOTg0ODE1MX0.-2HVXNtyOK_1-9g9wajCG0KWL1cMogkwIY61JlgwxeA"
 
 export const supabase = createClient(
-process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+ process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 // process.env.NEXT_PUBLIC_SUPABASE_URL!
