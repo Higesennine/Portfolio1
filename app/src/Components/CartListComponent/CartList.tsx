89c67a03c9d1e5884b0b80bcfd9a59678d5613e6
Higesennine <yasuhito.komano@gmail.com>
2024-10-14 15:29:00 -0700

    Replace SUPABASE_URL and ANON_KEY

--- app/src/Components/CartListComponent/CartList.tsx
+++ app/src/Components/CartListComponent/CartList.tsx
@@ -26,6 +26,7 @@ export default function CartList() {
         </li>
       ))}
     </ul>
+    
   </nav>
   )
 }
--- app/src/Components/ProductContainer/ProductContainer.tsx
+++ app/src/Components/ProductContainer/ProductContainer.tsx
@@ -24,12 +24,6 @@ export default function ProductContainer() {
                     <button id='addToCart-button' onClick={() => addToCart(product)}>Add to Cart</button>
                 </li>
             ))}
-            {/* <li>
-                <p id='productName'>Vans Old School</p>
-                <img src="https://images.vans.com/is/image/Vans/VN0A5FCB_B9M_HERO?wid=800&hei=1004&fmt=jpeg&qlt=50&resMode=sharp2&op_usm=0.9,1.5,8,0" alt="" />
-                <button>Product Detail</button>
-                <button>Add to Cart</button>
-            </li> */}
         </div>
     )
 }
